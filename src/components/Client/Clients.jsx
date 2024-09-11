6a617c8a5ef1f118c1dba0374951419bd1e19782
shahab khan <hussainmuhammad098123123@gmail.com>
2024-09-11 00:07:37 -0700

    commit

--- src/components/Client/Clients.jsx
+++ src/components/Client/Clients.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 import './client.css'
 const Clients = () => {
   return (
-    <div className='Con1'>Clients</div>
+    <div className='Con1'>
+       <h1 className='title'><span className='span1'>What</span> Our <span className='span2'>Clients Say</span></h1>
+       <p className='desc'>Real stories from real explorers – see how we’ve helped our clients create unforgettable journeys.</p>
+    </div>
   )
 }
 
